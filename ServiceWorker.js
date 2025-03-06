81da64875176496fb1f09401375e6c85e0de99d5
deronics <77541322+deronics@users.noreply.github.com>
2025-03-07 01:34:35 +1000

    Update ServiceWorker.js

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "do.Games-Space Waves-1.0-ms";
 const contentToCache = [
     "Build/dae5c7bd2b15915f51ae4e56b8f5367d.loader.js",
-    "Build/d216611ceb3e10d2934dbd58a8029ed7.framework.js.unityweb",
-    "Build/33181ac9c6b5e9231229e5ee92a26c0f.data.unityweb",
-    "Build/d8d5a6b0a903c8ed3da2920fb04ddc0d.wasm.unityweb",
+    "Build/c699bc4c24abfbdb4c8b3e15b0af7d3f.framework.js.unityweb",
+    "Build/568ea6b3af616bd57f419c508c933303.data.unityweb",
+    "Build/7fb5141de5923189bc1dc5f94eb619f2.wasm.unityweb",
     "TemplateData/style.css"
 
 ];
